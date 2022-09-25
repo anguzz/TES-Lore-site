e349240d44d550d0141f667cad1cea5ef4cb4ed4
Angel <santoyox714@gmail.com>
2022-09-24 22:07:35 -0700

    Update races.ts

--- src/lib/utils/races.ts
+++ src/lib/utils/races.ts
@@ -17,45 +17,52 @@ export const races = [
         id: 2,
         title: "Redguard",
         img: "races/redguard.jpeg",
-        descr:"From skyrim",
+        descr:"From Hammerfell",
      
     },
     {
         id: 3,
-        title: "High Elves",
-        img: "races/highElves.png",
-        descr:"From skyrim",
+        title: "Bretons",
+        img: "races/breton.jpeg",
+        descr:"From High Rock",
+     
     },
     {
         id: 4,
-        title: "Dark Elves",
-        img: "races/darkElves.jpg",
-        descr:"From skyrim",
+        title: "High Elves",
+        img: "races/highElves.png",
+        descr:"From The summerset Isles",
     },
     {
         id: 5,
-        title: "Wood elves",
-        img: "races/woodElves.jpg",
-        descr:"From skyrim",
+        title: "Dark Elves",
+        img: "races/darkElves.jpg",
+        descr:"From Morrowind",
     },
     {
         id: 6,
-        title: "Orcs",
-        img: "races/orcs.jpg",
-        descr:"From skyrim",
-       
+        title: "Wood elves",
+        img: "races/woodElves.jpg",
+        descr:"From Valenwood",
     },
     {
         id: 7,
-        title: "Argonians",
-        img: "races/argonians.jpg",
-        descr:"From skyrim",
+        title: "Orcs",
+        img: "races/orcs.jpg",
+        descr:"Wanderers/Orsinium",
+       
     },
     {
         id: 8,
+        title: "Argonians",
+        img: "races/argonians.jpg",
+        descr:"From Blackmarsh",
+    },
+    {
+        id: 9,
         title: "Khajit",
         img: "races/khajit.png",
-        descr:"From skyrim",
+        descr:"From Elsweyr",
     },
   
 ]
