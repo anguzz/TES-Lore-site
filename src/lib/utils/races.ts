6f07e51f84d5a52412b3da0230d3d09390eb7b3f
Angel <santoyox714@gmail.com>
2022-10-09 19:16:49 -0700

    Update races.ts

--- src/lib/utils/races.ts
+++ src/lib/utils/races.ts
@@ -3,66 +3,66 @@ export const races = [
         id: 0,
         title: "Nords",
         img: "races/nord.jpg",
-        descr:"From skyrim",
+        descr:"Province: Skyrim",
      
     },
     {
         id: 1,
         title: "Imperials",
         img: "races/imperial.jpg",
-        descr:"From Cyrodiil",
+        descr:"Province: Cyrodiil",
      
     },
     {
         id: 2,
         title: "Redguard",
         img: "races/redguard.jpg",
-        descr:"From Hammerfell",
+        descr:"Province: Hammerfell",
      
     },
     {
         id: 3,
         title: "Bretons",
         img: "races/breton.jpg",
-        descr:"From High Rock",
+        descr:"Province: High Rock",
      
     },
     {
         id: 4,
         title: "High Elves",
         img: "races/highElves.jpg",
-        descr:"From The summerset Isles",
+        descr:"Province: The summerset Isles",
     },
     {
         id: 5,
         title: "Dark Elves",
         img: "races/darkElves.jpg",
-        descr:"From Morrowind",
+        descr:"Province: Morrowind",
     },
     {
         id: 6,
         title: "Wood elves",
         img: "races/woodElves.jpg",
-        descr:"From Valenwood",
+        descr:"Province: Valenwood",
     },
     {
         id: 7,
         title: "Orcs",
         img: "races/orcs.jpg",
-        descr:"Wanderers/Orsinium",
+        descr:"Province: Wanderers/Orsinium",
        
     },
     {
         id: 8,
         title: "Argonians",
         img: "races/argonians.jpg",
-        descr:"From Blackmarsh",
+        descr:"Province: Blackmarsh",
     },
     {
         id: 9,
         title: "Khajit",
         img: "races/khajit.jpg",
-        descr:"From Elsweyr",
+        descr:"Province: Elsweyr",
     },
   
 ]
